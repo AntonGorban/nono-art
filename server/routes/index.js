b6ea2fb02b1784fd81f4d668c88b8211131e9a41
Anton Gorban <antogor.work@gmail.com>
2022-08-27 18:03:28 +0300

    update expo workspace & add ts

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const levelRouter = require("./levelRouter");
-
-router.use("/level", levelRouter);
-
-module.exports = router;
--- server/routes/levelRouter.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const LevelController = require("../controllers/LevelController");
-
-router.post("/sharing", LevelController.sharing);
-
-module.exports = router;
