89917ea1795032236d6458642a159920929d8829
Anton Gorban <antogor.work@gmail.com>
2022-09-24 15:01:38 +0300

    fix: font loading

--- App.jsx
+++ App.jsx
@@ -4,59 +4,59 @@ import { storage, getLevelsFromRepo } from './src/storage';
 
 import { connect } from 'react-redux';
 
-class AppContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.setLevels = props.setLevels;
-    this.pushNewLevels = props.pushNewLevels;
-    this.setSelectedLevel = props.setSelectedLevel;
-    this.setDesignerState = props.setDesignerState;
-  }
+// class AppContainer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.setLevels = props.setLevels;
+//     this.pushNewLevels = props.pushNewLevels;
+//     this.setSelectedLevel = props.setSelectedLevel;
+//     this.setDesignerState = props.setDesignerState;
+//   }
 
-  async componentDidMount() {
-    await Promise.all([
-      storage.getObj('levels').then((data) => data !== null && this.setLevels(data)),
+//   async componentDidMount() {
+//     await Promise.all([
+//       storage.getObj('levels').then((data) => data !== null && this.setLevels(data)),
 
-      storage
-        .getObj('selectedLevel')
-        .then((data) =>
-          data !== null ? this.setSelectedLevel(data) : storage.setObj('selectedLevel', this.props.selectedLevel)
-        ),
+//       storage
+//         .getObj('selectedLevel')
+//         .then((data) =>
+//           data !== null ? this.setSelectedLevel(data) : storage.setObj('selectedLevel', this.props.selectedLevel)
+//         ),
 
-      storage.getObj('designer').then((data) => data !== null && this.setDesignerState(data)),
-    ]);
+//       storage.getObj('designer').then((data) => data !== null && this.setDesignerState(data)),
+//     ]);
 
-    await getLevelsFromRepo().then((repoLevels) => {
-      if (repoLevels.length > this.props.levels.length) {
-        let newLevels = [];
-        for (let i = this.props.levels.length; i < repoLevels.length; i++) {
-          newLevels.push(repoLevels[i]);
-        }
-        this.pushNewLevels(newLevels);
-        console.log('getLevelsFromRepo', newLevels);
-      }
-    });
-  }
+//     await getLevelsFromRepo().then((repoLevels) => {
+//       if (repoLevels.length > this.props.levels.length) {
+//         let newLevels = [];
+//         for (let i = this.props.levels.length; i < repoLevels.length; i++) {
+//           newLevels.push(repoLevels[i]);
+//         }
+//         this.pushNewLevels(newLevels);
+//         console.log('getLevelsFromRepo', newLevels);
+//       }
+//     });
+//   }
 
-  render() {
-    return null;
-  }
-}
+//   render() {
+//     return null;
+//   }
+// }
 
-const mapStateToProps = (state) => ({
-  levels: state.levels.levels,
-  selectedLevel: state.levels.selectedLevel,
-});
+// const mapStateToProps = (state) => ({
+//   levels: state.levels.levels,
+//   selectedLevel: state.levels.selectedLevel,
+// });
 
-import { setLevelsAC, pushNewLevelsAC, setSelectedLevelAC } from './src/redux/levelsReducer';
+// import { setLevelsAC, pushNewLevelsAC, setSelectedLevelAC } from './src/redux/levelsReducer';
 
-import { setDesignerStateAC } from './src/redux/designerReducer';
+// import { setDesignerStateAC } from './src/redux/designerReducer';
 
-const mapDispatchToProps = (dispatch) => ({
-  setLevels: (levels) => dispatch(setLevelsAC(levels)),
-  pushNewLevels: (levels) => dispatch(pushNewLevelsAC(levels)),
-  setSelectedLevel: (selectedLevel) => dispatch(setSelectedLevelAC(selectedLevel)),
-  setDesignerState: (state) => dispatch(setDesignerStateAC(state)),
-});
+// const mapDispatchToProps = (dispatch) => ({
+//   setLevels: (levels) => dispatch(setLevelsAC(levels)),
+//   pushNewLevels: (levels) => dispatch(pushNewLevelsAC(levels)),
+//   setSelectedLevel: (selectedLevel) => dispatch(setSelectedLevelAC(selectedLevel)),
+//   setDesignerState: (state) => dispatch(setDesignerStateAC(state)),
+// });
 
-const App = connect(mapStateToProps, mapDispatchToProps)(AppContainer);
+// const App = connect(mapStateToProps, mapDispatchToProps)(AppContainer);
--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 import { Provider } from 'react-redux';
 
-import { useAppSelector } from './src/hooks/services/store/store';
 import { useDesignerActions } from './src/hooks/services/store/useDesignerActions';
 import { useLevelsActions } from './src/hooks/services/store/useLevelsActions';
 import { Screens } from './src/screens/Screens';
@@ -17,26 +16,17 @@ const App: React.FC = () => {
   const { setState: setDesignerState } = useDesignerActions();
   const { fetchLevels, getLevelsFromStorage } = useLevelsActions();
 
-  const state = useAppSelector((state) => state.levels);
-
   useEffect(() => {
     storage.getObj('designer').then((data) => {
       if (data !== null) setDesignerState(data);
     });
-  }, []);
+  }, [setDesignerState]);
 
   useEffect(() => {
     getLevelsFromStorage().finally(() => fetchLevels());
-  }, []);
+  }, [fetchLevels, getLevelsFromStorage]);
 
-  console.log('state', state);
-
-  return (
-    <>
-      <StatusBar style="light" backgroundColor={Color.dark} translucent={false} />
-      <Screens />
-    </>
-  );
+  return <Screens />;
 };
 
 SplashScreen.preventAutoHideAsync();
@@ -44,30 +34,42 @@ SplashScreen.preventAutoHideAsync();
 const store = setupStore();
 
 export default function () {
-  const [appIsReady, setAppIsReady] = useState<boolean>(false);
-
   /* ---------------------------------- Fonts --------------------------------- */
 
+  const [fontsIsLoaded] = Font.useFonts({
+    [FontFamily.montserratAltLight]: require('./assets/fonts/MontserratAlternates-Light.otf'),
+    [FontFamily.montserratAltRegular]: require('./assets/fonts/MontserratAlternates-Regular.otf'),
+    [FontFamily.montserratAltBold]: require('./assets/fonts/MontserratAlternates-Bold.otf'),
+    [FontFamily.frederickaTheGreat]: require('./assets/fonts/FrederickatheGreat-Regular.otf'),
+  });
+
   useEffect(() => {
-    Font.loadAsync({
-      [FontFamily.montserratAltLight]: require('./assets/fonts/MontserratAlternates-Light.otf'),
-      [FontFamily.montserratAltRegular]: require('./assets/fonts/MontserratAlternates-Regular.otf'),
-      [FontFamily.montserratAltBold]: require('./assets/fonts/MontserratAlternates-Bold.otf'),
-      [FontFamily.frederickaTheGreat]: require('./assets/fonts/FrederickatheGreat-Regular.otf'),
-    }).then(() => setAppIsReady(true));
+    const prepare = async () => {
+      await SplashScreen.preventAutoHideAsync();
+    };
+    prepare();
   }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsIsLoaded]);
+
+  useEffect(() => {
+    onLayoutRootView();
+  }, [onLayoutRootView]);
 
   /* --------------------------------- / Fonts -------------------------------- */
 
-  useEffect(() => {
-    if (appIsReady) SplashScreen.hideAsync();
-  }, [appIsReady]);
-
-  if (!appIsReady) return null;
+  if (!fontsIsLoaded) {
+    return null;
+  }
 
   return (
     <Provider store={store}>
-      <App />;
+      <StatusBar style="light" backgroundColor={Color.dark} translucent={false} />
+      <App />
     </Provider>
   );
 }
